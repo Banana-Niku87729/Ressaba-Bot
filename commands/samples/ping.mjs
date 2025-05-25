3c1af853a69148c435aa7d898614a62baf635b85
Banana_Niku87729 <130275367+Banana-Niku87729@users.noreply.github.com>
2025-05-26 06:34:14 +0900

    Delete commands directory

--- commands/samples/ping.mjs
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('Botが作動しているかの確認');
-
-export async function execute(interaction){
-	await interaction.reply('✅作動しています');
-}
